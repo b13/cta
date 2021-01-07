e71378acaaf431a1d1457e8bf02fb606065479a7
David Steeb <david.steeb@b13.de>
2021-01-07 17:14:52 +0100

    [TASK] Add misc. code cleanups

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,48 +1,24 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "cta".
- *
- * Generated 13-12-2017 13:13
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array (
     'title' => 'Content-type "CTA"',
     'description' => 'Adds a CTA Content-type to tt_content',
     'category' => 'backend',
-    'shy' => 0,
     'version' => '1.0.0',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'modify_tables' => 'tt_content',
     'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'David Steeb, b13 GmbH',
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
-    'constraints' =>
-        array (
-            'depends' =>
-                array (
-                    'php' => '5.3.7-0.0.0',
-                    'fluid_styled_content' => '',
-                ),
-            'conflicts' =>
-                array (
-                    'css_styled_content' => '',
-                ),
-            'suggests' =>
-                array (
-                ),
-        ),
+    'constraints' => [
+        'depends' =>
+            array (
+                'fluid_styled_content' => '*',
+            ),
+        'conflicts' => [],
+        'suggests' => []
+    ]
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('TYPO3_MODE') or die();
+
 /*
  * This file is part of the package b13/cta.
  *
@@ -7,11 +9,11 @@
  * LICENSE file that was distributed with this source code.
  */
 
-defined('TYPO3_MODE') || die();
-
-$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-$iconRegistry->registerIcon(
-    'ctype-cta',
-    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-    ['source' => 'EXT:cta/Resources/Public/Icons/Cta.svg']
-);
+call_user_func(static function () {
+    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+    $iconRegistry->registerIcon(
+        'ctype-cta',
+        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        ['source' => 'EXT:cta/Resources/Public/Icons/Cta.svg']
+    );
+});
