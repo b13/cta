5c342a30efa91a7f266ce5cb812cb7c4c01bc66f
David Steeb <david.steeb@b13.com>
2021-05-23 13:06:42 +0200

    [TASK] Update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,24 +1,21 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array (
-    'title' => 'Content-type "CTA"',
-    'description' => 'Adds a CTA Content-type to tt_content',
+    'title' => 'Content type "CTA"',
+    'description' => 'Adds a CTA content type to tt_content',
     'category' => 'backend',
     'version' => '1.1.2',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
     'modify_tables' => 'tt_content',
-    'clearcacheonload' => 0,
+    'clearCacheOnLoad' => 0,
     'author' => 'David Steeb, b13 GmbH',
     'author_email' => 'typo3@b13.com',
     'author_company' => 'b13 GmbH',
     'constraints' => [
-        'depends' =>
-            array (
-                'fluid_styled_content' => '*',
-            ),
-        'conflicts' => [],
-        'suggests' => []
+        'depends' => [
+            'fluid_styled_content' => '*',
+        ]
     ]
 );
